73b9b45d59928af1b86272ef30e58b018e9b3c1a
Abdohm-coder <hadjmedabdo@gmail.com>
2023-05-01 19:17:23 +0100

    Create a store and article Api using redux toolkit

---
+++ src/services/article.ts
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+const options = {
+  method: "GET",
+  url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+  params: {
+    url: "https://time.com/6266679/musk-ai-open-letter/",
+    length: "3",
+  },
+  headers: {
+    "X-RapidAPI-Key": "d1f528aa6amshd72a9753d166747p1ef544jsnf08383f892e5",
+    "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+  },
+};
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
